2d24ab91e521354333a30a844df62733778d3d53
SANO Masatoshi <snmsts@gmail.com>
2016-09-07 02:44:18 +0900

    separate file

---
+++ scripts/qemu-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Command used to run the tests
+TEST_COMMAND="make test"
+CHROOT_DIR=/tmp/arm-chroot
+
+function run_test {
+    echo "Running tests"
+    echo "Environment: $(uname -a)"
+
+    ${TEST_COMMAND}
+}
+
+if [ -e "/.chroot_is_done" ]; then
+    # We are inside ARM chroot
+    echo "Running inside chrooted environment"
+
+    . ./envvars.sh
+    run_test
+else
+    if [ "${ARCH}" = "arm" ]; then
+        # Call ourselves again which will cause tests to run
+        sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./scripts/qemu-test.sh"
+    else
+        run_test
+    fi
+fi
+
